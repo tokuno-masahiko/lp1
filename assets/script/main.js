bbf2934d1699d282ec24994013733888b8742feb
tokuno-masahiko <tokuno5956@gmail.com>
2024-06-03 21:31:34 +0900

    初稿後の修正2

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,17 +8,17 @@ $(function(){
     }
   });
 
-  $(".sec3_tag2").click(function(){
-    $(".sec3_wrapper1").css('display', 'none');
-    $(".sec3_wrapper2").css('display', 'block');
-    $(".sec3_tag2").css('background-color', '#f2f9ff');
-    $(".sec3_tag1").css('background-color', '#f0f0f0');
+  $(".section3_tag2").click(function(){
+    $(".section3_wrapper1").css('display', 'none');
+    $(".section3_wrapper2").css('display', 'block');
+    $(".section3_tag2").css('background-color', '#f2f9ff');
+    $(".section3_tag1").css('background-color', '#f0f0f0');
   });
 
-  $(".sec3_tag1").click(function(){
-    $(".sec3_wrapper2").css('display', 'none');
-    $(".sec3_wrapper1").css('display', 'block');
-    $(".sec3_tag1").css('background-color', '#f2f9ff');
-    $(".sec3_tag2").css('background-color', '#f0f0f0');
+  $(".section3_tag1").click(function(){
+    $(".section3_wrapper2").css('display', 'none');
+    $(".section3_wrapper1").css('display', 'block');
+    $(".section3_tag1").css('background-color', '#f2f9ff');
+    $(".section3_tag2").css('background-color', '#f0f0f0');
   });
 });
