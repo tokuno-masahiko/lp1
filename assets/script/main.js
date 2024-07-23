18366a998a188ea8a53c7d0d26cdefa2f7ed90d6
tokuno-masahiko <tokuno5956@gmail.com>
2024-07-23 20:55:13 +0900

    修正の模擬案件レビューの修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,24 +1,24 @@
-$(function(){
-  $(".js_menu_bar").click(function(){
-    const navMenu = $('.sp_nav_menu')
-    if(navMenu.css('display') === 'none'){
-      navMenu.fadeIn();
-    }else{
-      navMenu.fadeOut();
-    }
-  });
+$(function () {
+    $(".js_menu_bar").click(function () {
+        const navMenu = $('.sp_nav_menu')
+        if (navMenu.css('display') === 'none') {
+            navMenu.fadeIn();
+        } else {
+            navMenu.fadeOut();
+        }
+    });
 
-  $(".section3_tag2").click(function(){
-    $(".section3_wrapper1").css('display', 'none');
-    $(".section3_wrapper2").css('display', 'block');
-    $(".section3_tag2").css('background-color', '#f2f9ff');
-    $(".section3_tag1").css('background-color', '#f0f0f0');
-  });
+    $(".section3_tag2").click(function () {
+        $(".section3_wrapper1").css('display', 'none');
+        $(".section3_wrapper2").css('display', 'block');
+        $(".section3_tag2").css('background-color', '#f2f9ff');
+        $(".section3_tag1").css('background-color', '#f0f0f0');
+    });
 
-  $(".section3_tag1").click(function(){
-    $(".section3_wrapper2").css('display', 'none');
-    $(".section3_wrapper1").css('display', 'block');
-    $(".section3_tag1").css('background-color', '#f2f9ff');
-    $(".section3_tag2").css('background-color', '#f0f0f0');
-  });
+    $(".section3_tag1").click(function () {
+        $(".section3_wrapper2").css('display', 'none');
+        $(".section3_wrapper1").css('display', 'block');
+        $(".section3_tag1").css('background-color', '#f2f9ff');
+        $(".section3_tag2").css('background-color', '#f0f0f0');
+    });
 });
